f6d724c6f9fb0dba1939a847d1246bcb729e8061
Alexey Kobyleha <uazonner@gmail.com>
2016-10-26 21:02:38 +0300

    added checkboxes

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -4,29 +4,33 @@
     <div class="container">
         <h4>Vocabulary words</h4>
         <div class="row">
+            {!! Form::open(['url' => 'foo/bar']) !!}
             <div class="col-md-6">
-                {!! Form::open(['url' => 'foo/bar']) !!}
-                        <ul class="list-group">
-                        @foreach($vocabulary as $item)
-                            <li class="list-group-item">
-                                {!! Form::checkbox('name', 'value') !!}
-                                {{ $item->word }}
-                            </li>
-                        @endforeach
-                        </ul>
-                {!! Form::submit('Generate!') !!}
-                {!! Form::close() !!}
+                <ul class="list-group">
+                    @foreach($vocabulary as $item)
+                        <li class="list-group-item">
+                            {!! Form::checkbox('name', 'value') !!}
+                            {{ $item->word }}
+                        </li>
+                    @endforeach
+                </ul>
                 {{ $vocabulary->render() }}
             </div>
             <div class="col-md-6">
                 <div class="panel panel-default">
                     <div class="panel-heading">Tools</div>
-
                     <div class="panel-body">
-                        Generate!
+                        <h5>Select hashing</h5>
+                        <p>{!! Form::checkbox('name', 'value') !!} MD5</p>
+                        <p>{!! Form::checkbox('name', 'value') !!} SHA-256</p>
+                        <p>{!! Form::checkbox('name', 'value') !!} SHA-512</p>
+                        <p>{!! Form::checkbox('name', 'value') !!} STD DES</p>
+                        <p>{!! Form::checkbox('name', 'value') !!} BLOWFISH</p>
+                        {!! Form::submit('Create hash', ['class' => 'btn btn-primary btn-sm']) !!}
                     </div>
                 </div>
             </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
