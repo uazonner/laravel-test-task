0c2f13f18a044852de890d9ca049a7d3edba0928
Alexey Kobyleha <uazonner@gmail.com>
2016-10-26 19:49:39 +0300

    add html form and edit list view

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -4,13 +4,28 @@
     <div class="container">
         <h4>Vocabulary words</h4>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-6">
+                {!! Form::open(['url' => 'foo/bar']) !!}
                         <ul class="list-group">
                         @foreach($vocabulary as $item)
-                            <li class="list-group-item">{{ $item->word }}</li>
+                            <li class="list-group-item">
+                                {!! Form::checkbox('name', 'value') !!}
+                                {{ $item->word }}
+                            </li>
                         @endforeach
                         </ul>
+                {!! Form::submit('Generate!') !!}
+                {!! Form::close() !!}
                 {{ $vocabulary->render() }}
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Tools</div>
+
+                    <div class="panel-body">
+                        Generate!
+                    </div>
+                </div>
             </div>
         </div>
     </div>
